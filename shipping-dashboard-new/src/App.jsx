a5fda267ded4d1c3f1a2fcaf76559310ab355e3f
pleasurestechno <jsndoll123@gmail.com>
2026-03-14 02:45:54 -0700

    Initial commit for new project

---
+++ shipping-dashboard-new/src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+import ShippingDashboard from './components/ShippingDashboard';
+
+function App() {
+  return (
+    <div className="App bg-gradient-to-br from-primary-hover to-primary text-white min-h-screen flex items-center justify-center p-4">
+      <ShippingDashboard />
+    </div>
+  );
+}
+
+export default App;
